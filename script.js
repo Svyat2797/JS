1e77b85f65e8fd2fd06eeccc511e4f22ea2fa347
Slava <example@gmail.com>
2021-12-23 23:45:16 +0300

    train 4 Odject

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 
 'use strict';
 
-let numberOfFilms;
+/* let numberOfFilms;
 
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -90,4 +90,28 @@ function  writeYourGenres() {
         personalMovieDB[i-1] = genre;
     }
 }
-writeYourGenres(); 
+writeYourGenres();  */
+
+const options = {
+    name:"test",
+    width: 1024,
+    height: 500,
+    colors: {
+        border: "black",
+        bg: "red"
+    }
+
+};
+console.log(Object.keys(options).length); 
+
+
+/* for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log( `Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log( `Свойство ${key} имеет значение ${options[key]}`);
+    }
+    
+} */
